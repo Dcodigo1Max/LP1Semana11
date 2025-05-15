90fb1fff20a957f9a157e069acb09853b929b2f9
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-15 18:33:59 +0100

    Made Controller

---
+++ PlayerManagerMVC/Controller.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PlayerManager4
+{
+    
+    public class Controller 
+    {
+        
+    }
+}
