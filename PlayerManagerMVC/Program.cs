e590f1a6c38a2fcd4166b267e55da789d411448c
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-15 19:03:15 +0100

    New changes in controller and Program

--- PlayerManagerMVC/Program.cs
+++ PlayerManagerMVC/Program.cs
@@ -11,14 +11,6 @@ namespace PlayerManager4
     /// </summary>
     public class Program
     {
-        /// The list of all players
-        private readonly PlayersList playerList;
-
-        // Comparer for comparing player by name (alphabetical order)
-        private readonly IComparer<Player> compareByName;
-
-        // Comparer for comparing player by name (reverse alphabetical order)
-        private readonly IComparer<Player> compareByNameReverse;
 
         /// <summary>
         /// Program begins here.
@@ -26,146 +18,25 @@ namespace PlayerManager4
         /// <param name="args">Not used.</param>
         private static void Main(string[] args)
         {
-            // Create a new instance of the player listing program
-            Program prog = new Program();
-            // Start the program instance
-            prog.Start();
-        }
-
-        /// <summary>
-        /// Creates a new instance of the player listing program.
-        /// </summary>
-        private Program()
-        {
+              //Inicialize player comparers
             // Initialize player comparers
-            compareByName = new CompareByName(true);
-            compareByNameReverse = new CompareByName(false);
+            IComparer<Player>compareByName = new CompareByName(true);
+            IComparer<Player>compareByNameReverse = new CompareByName(false);
 
-            // Initialize the player list with two players using collection
+        // Initialize the player list with two players using collection
             // initialization syntax
-            playerList = new PlayersList() {
+            PlayersList playerList = new PlayersList() {
                 new Player("Marco", 100),
                 new Player("Polo", 500)
             };
+           
 
-            view = new UglyView();
-
-
-        }
-
-        /// <summary>
-        /// Start the player listing program instance
-        /// </summary>
-        private void Start()
-        {
-            // We keep the user's option here
-            string option;
-
-            // Main program loop
-            do
-            {
-                // Show menu and get user option
-                option = view.MainMenu();
-
-                // Determine the option specified by the user and act on it
-                switch (option)
-                {
-                    case "1":
-                        // Insert player
-                        InsertPlayer();
-                        break;
-                    case "2":
-                        view.ShowPlayers(playerList);
-                        break;
-                    case "3":
-                        ListPlayersWithScoreGreaterThan();
-                        break;
-                    case "4":
-                        SortPlayerList();
-                        break;
-                    case "0":
-                        view.ShowGoodbyeMessage();
-                        break;
-                    default:
-                        view.ShowInvalidOptionMessage();
-                        break;
-                }
-
-                // Wait for user to press a key...
-                view.WaitForUser();
-
-                // Loop keeps going until players choses to quit (option 4)
-            } while (option != "0");
-        }
-
-        /// <summary>
-        /// Shows the main menu.
-        /// </summary>
-    
-
-        /// <summary>
-        /// Inserts a new player in the player list.
-        /// </summary>
-        private void InsertPlayer()
-        {
-            Player newPlayer = view.AskForPlayerInfo();
-            playerList.Add(newPlayer);
-        }
-
-    
-
-        /// <summary>
-        /// Show all players with a score higher than a user-specified value.
-        /// </summary>
-        private void ListPlayersWithScoreGreaterThan()
-        {
-            
-            // Enumerable of players with score higher than the minimum score
-            IEnumerable<Player> playersWithScoreGreaterThan;
-
-            // Minimum score user should have in order to be shown
-            int minScore = view.AskForMinScore();
-
-            // Get players with score higher than the user-specified value
-            playersWithScoreGreaterThan =
-                playerList.GetPlayersWithScoreGreaterThan(minScore);
-
-            // List all players with score higher than the user-specified value
-            view.ShowPlayers(playersWithScoreGreaterThan);
-        }
-
-        /// <summary>
-        /// Get players with a score higher than a given value.
-        /// </summary>
-        /// <param name="minScore">Minimum score players should have.</param>
-        /// <returns>
-        /// An enumerable of players with a score higher than the given value.
-        /// </returns>
-       
-        /// <summary>
-        ///  Sort player list by the order specified by the user.
-        /// </summary>
-        private void SortPlayerList()
-        {
-            PlayerOrder playerOrder = view.AskForPlayerOrder();
-
-            switch (playerOrder)
-            {
-                case PlayerOrder.ByScore:
-                    playerList.Sort();
-                    break;
-                case PlayerOrder.ByName:
-                    playerList.Sort(compareByName);
-                    break;
-                case PlayerOrder.ByNameReverse:
-                    playerList.Sort(compareByNameReverse);
-                    break;
-                default:
-                    view.ShowInvalidOptionMessage();
-                    break;
-            }
         }
     }
-}
 
+}
 //Console.Error.WriteLine("\n>>> Unknown player order! <<<\n");
+ // Create a new instance of the player listing program
+         //   Program prog = new Program();
+            // Start the program instance
+          //  prog.Start();
