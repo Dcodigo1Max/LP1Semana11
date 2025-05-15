78f08d262dec5d983f2357f3b9311cfcc7ec2d91
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-05-15 19:42:39 +0100

    Namespace Change PlayersList

--- PlayerManagerMVC/PlayersList.cs
+++ PlayerManagerMVC/PlayersList.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using PlayerManager4;
 
-namespace PlayerManagerMV4
+
+namespace PlayerManagerMV
 {
     public class PlayersList : List<Player>
     {
